ecec0c261392fae2f01afacc3ea12fc19729e992
Hieu Tran <tr.kimhieu@gmail.com>
2022-09-27 11:47:25 +0000

    Add Bins strategy

--- load_env.sh
+++ load_env.sh
@@ -6,6 +6,7 @@ export PYTHONUSERBASE="/home/ftuser/.local"
 export LIVE_DATABASE_URL=${LIVE_DATABASE_URL//postgres/postgresql}
 export DRY_DATABASE_URL=${DRY_DATABASE_URL//postgres/postgresql}
 
+envsubst < user_data/config.live-futures.json > live-futures.json
 envsubst < user_data/config.live.json > live.json
 envsubst < user_data/config.dry.json > dry.json
 envsubst < user_data/config.test.json > test.json
